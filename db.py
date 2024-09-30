f77f745e1bfc57e51bc7dc168b54c29012965839
Richard Bond <brycerbond@gmail.com>
2024-09-30 16:37:01 -0500

    added db queries

--- db.py
+++ db.py
@@ -1,11 +1,19 @@
 import sqlite3
 print('starting')
-con = sqlite3.connect("tutorial.db")
 
+# creates db or conntet to it 
+con = sqlite3.connect("tutorial.db")
+# create cursor to do queries in db 
+cur = con.cursor()
 
 # create a table
 
-
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
+  )""")
 
 # create a second table with keys referencing the first 
 
@@ -13,12 +21,35 @@ con = sqlite3.connect("tutorial.db")
 # CRUD
 
 # Create
+# Insert 1: 
+cur.execute("""
+    INSERT into users (first_name, last_name) values ("Danielle", "W")
+""")
+
+cur.execute("""
+    INSERT into users (first_name, last_name) values ("Bryce", "Bond")
+""")
+
+con.commit()
 
 # Read
 
-# Update
+res = cur.execute("""
+    Select * from users
+""")
 
+print(res.fetchall())
+
+# Update
+cur.execute("""
+                  Update users set last_name = 'yells' where first_name = 'Danielle'
+                  """)
+con.commit()
+
+print(res.fetchall())
 # Delete
+
+
 
 
 
