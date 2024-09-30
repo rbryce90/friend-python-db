c31eb634928d5d55d93fe3091f560f45baa29183
Richard Bond <brycerbond@gmail.com>
2024-09-30 15:33:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# friend-python-db
---
+++ db.py
@@ -0,0 +1,23 @@
+import sqlite3
+print('starting')
+con = sqlite3.connect("tutorial.db")
+
+
+# create a table
+
+# create a second table with keys referencing the first 
+
+
+# CRUD
+
+# Create
+
+# Read
+
+# Update
+
+# Delete
+
+
+
+print('The End')
